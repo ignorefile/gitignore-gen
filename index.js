b2a55ae7288e267910b3bd78b2f5f5ac0cb42cf2
Leigh Zhu <i@zhuli.me>
2014-07-09 15:19:30 +0800

    re-gen .gitignore by myself :P && add command support

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
 
 var fs = require('fs');
 
+var cmd = require('./lib/cmd-parse');
 var tpls = require('./lib/tpl.json');
 var keywords = process.argv.slice(2);
 
-var result = keywords.reduce(function(prev, curr, idx, arr) {
+var options = cmd();
+
+var result = options.append.reduce(function(prev, curr, idx, arr) {
     if(keywords.indexOf(curr) >= 0) 
     return prev.concat(tpls[curr])
 }, [])
 
 var ws = fs.createWriteStream('./.gitignore', {
-    flags: 'a'
+    flags: options.force ? 'w' : 'a'
 })
+
+if(options.custom) {
+    options.custom.forEach(function(itm) {
+        ws.write(itm + '\n');
+    })
+}
 
 ws.end(result.join(''));
